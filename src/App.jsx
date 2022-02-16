9d83e0b1305190f54a4346afc84fe05f71ecc944
macrosDevelop <99655300+macrosDevelop@users.noreply.github.com>
2022-02-16 11:37:37 +0100

    mod app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import ApiSearchRecipe from "./component/ApiSearchRecipe";
 
 function App() {
   return (
     <div className="App">
       <h1>hello world!</h1>
+      <ApiSearchRecipe />
     </div>
   );
 }
