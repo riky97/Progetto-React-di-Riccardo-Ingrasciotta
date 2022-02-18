6b4d6307ac7b95714cc6da8ef520792e5c06370e
riky97 <71893803+riky97@users.noreply.github.com>
2022-02-18 13:12:31 +0100

    Modified background layout and implemented search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Recipes from "./component/Recipes";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Layout, Breadcrumb } from "antd";
+import { Layout, Breadcrumb, BackTop } from "antd";
 import Navbar from "./component/Navbar";
 import FooterWeb from "./FooterWeb";
 import "antd/dist/antd.css";
 import axios from "axios";
+import { UpCircleOutlined } from "@ant-design/icons";
 
 const { Content, Footer, Header } = Layout;
 const apiKey = "e9a74a3703a74b43b3d8f2c5b3af6879";
@@ -34,24 +35,29 @@ function App() {
     }
   };
   return (
-    <Layout className="layout">
-      <Navbar />
-      <Content style={{ padding: "0 50px" }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content">
-          <Router>
-            <Routes>
-              <Route exact path="/" element={<Recipes recipe={recipes} />} />
-            </Routes>
-          </Router>
-        </div>
-      </Content>
-      <FooterWeb />
-    </Layout>
+    <>
+      <Layout className="layout">
+        <Navbar />
+        <Content style={{ padding: "0 50px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">
+            <Router>
+              <Routes>
+                <Route exact path="/" element={<Recipes recipe={recipes} />} />
+              </Routes>
+            </Router>
+          </div>
+        </Content>
+        <FooterWeb />
+      </Layout>
+      <BackTop>
+        <UpCircleOutlined className="back-top" />
+      </BackTop>
+    </>
   );
 }
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { Layout, Menu, Input } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
 const Header = Layout;
 const { Search } = Input;
 
 const Navbar = () => {
   const onSearch = (value) => console.log(value);
   return (
-    <Header>
-      <div className="logo" />
-      <Menu mode="horizontal" className="d-flex justify-content-center header">
+    <Header className="d-grid">
+      <Menu mode="horizontal" className="header">
+        <div className="logo">
+          {" "}
+          <h2>Vegetarian Recipe</h2>
+        </div>
         <Search
-          style={{ maxWidth: "40%" }}
+          className="search-bar"
           placeholder="input search text"
           onSearch={onSearch}
-          enterButton
+          enterButton="Filters"
           size="large"
         />
+        <div className="home">
+          <a href="/" className="a-home">
+            {" "}
+            <HomeOutlined />{" "}
+          </a>
+        </div>
       </Menu>
     </Header>
   );
